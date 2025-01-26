8b6b70fc5252ed5ed1c1c1d63d05c08c9547b871
immament <immament@gmail.com>
2025-01-26 16:10:06 +0100

    style: match result

--- src/app/MediaHeader/MediaHeader.component.tsx
+++ src/app/MediaHeader/MediaHeader.component.tsx
@@ -30,20 +30,30 @@ export class MediaHeaderComponent {
     return (
       <div class="mv-media-header">
         <div class="mv-content">
-          <div class="mv-team">
-            <div class="mv-team-1 mv-box">
-              <div>{this._match.homeTeam.name}</div>
+          <div></div>
+          <div class="mv-team mv-team-1">
+            <div class="mv-team-1 mv-box mv-team-name">
+              <div>
+                {this._match.homeTeam.name} {this._match.homeTeam.name}{" "}
+                {this._match.homeTeam.name} {this._match.homeTeam.name}{" "}
+                {this._match.homeTeam.name} {this._match.homeTeam.name}{" "}
+              </div>
             </div>
             <div class="mv-team-1 mv-box" ref={this._homeTeamGoalsRef}>
               0
             </div>
           </div>
-          <div class="mv-team">
+          <div class="mv-team mv-team-2">
             <div class="mv-team-2 mv-box" ref={this._awayTeamGoalsRef}>
               0
             </div>
-            <div class="mv-team-2 mv-box">
-              <div>{this._match.awayTeam.name}</div>
+            <div class="mv-team-2 mv-box mv-team-name">
+              <div>
+                {this._match.awayTeam.name} {this._match.awayTeam.name}{" "}
+                {this._match.awayTeam.name} {this._match.awayTeam.name}{" "}
+                {this._match.awayTeam.name} {this._match.awayTeam.name}{" "}
+                {this._match.awayTeam.name}{" "}
+              </div>
             </div>
           </div>
           <div class="mv-time mv-box" ref={this._timeRef}>
